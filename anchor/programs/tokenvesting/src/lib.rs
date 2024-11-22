bb432b4cecc3cadda17a674342b8f9f8ff88ab5c
Nidhish <nidhish0430@gmail.com>
2024-11-22 15:40:40 +0530

    first commit

---
+++ anchor/programs/tokenvesting/src/lib.rs
@@ -0,0 +1,167 @@
+use anchor_lang::prelude::*;
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token_interface::{ self, Mint, TokenAccount, TokenInterface, TransferChecked };
+
+
+declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
+
+#[program]
+pub mod tokenvesting {
+    use super::*;
+
+    pub fn create_vesting_account(
+        ctx: Context<CreateVestingAccount>,
+        company_name: String
+    ) -> Result<()> {
+        *ctx.accounts.vesting_account = VestingAccount {
+            owner: ctx.accounts.signer.key(),
+            mint: ctx.accounts.mint.key(),
+            treasury_token_account: ctx.accounts.treasury_token_account.key(),
+            company_name,
+            treasury_bump: ctx.bumps.treasury_token_account,
+            bump: ctx.bumps.vesting_account,
+        };
+
+        Ok(())
+    }
+
+    pub fn create_employee_account(
+        ctx : Context<CreateEmployeeAccount>,
+        start_time : i64,
+        end_time : i64,
+        total_amount : i64,
+        cliff_time : i64
+    ) -> Result<()>{
+        *ctx.accounts.employee_account = EmployeeAccount{
+            beneficiary : ctx.accounts.beneficiary.key(),
+            start_time,
+            end_time,
+            total_amount,
+            total_withdrawn : 0,
+            cliff_time,
+            vesting_account : ctx.accounts.vesting_account.key(),
+            bump : ctx.bumps.employee_account
+        };
+        Ok(())
+    }
+
+    pub fn claim_tokens(ctx: Context<ClaimTokens>, _company_name: String) -> Result<()> {
+        // The main logic lies here
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(company_name: String)]
+pub struct CreateVestingAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        space = 8 + VestingAccount::INIT_SPACE,
+        payer = signer,
+        seeds = [company_name.as_ref()],
+        bump
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        init,
+        token::mint = mint,
+        token::authority = treasury_token_account,
+        payer = signer,
+        seeds = [b"vesting_treasury", company_name.as_bytes()],
+        bump
+    )]
+    pub treasury_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CreateEmployeeAccount<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,  // Employer(signer is the owner since they will claim it later on)
+    pub beneficiary: SystemAccount<'info>,
+    #[account(has_one = owner)] // for validating that the person with the right access
+    pub vesting_account: Account<'info, VestingAccount>,  // we need the vesting account state information
+    #[account(
+        init,
+        space = 8 + EmployeeAccount::INIT_SPACE,
+        payer = owner,
+        seeds = [b"employee_vesting", beneficiary.key().as_ref(), vesting_account.key().as_ref()],
+        bump
+    )]  // creating the employee account
+    pub employee_account: Account<'info, EmployeeAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(company_name: String)]
+pub struct ClaimTokens<'info> {
+    #[account(mut)]
+    pub beneficiary: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"employee_vesting", beneficiary.key().as_ref(), vesting_account.key().as_ref()],
+        bump = employee_account.bump,
+        has_one = beneficiary,
+        has_one = vesting_account
+    )]
+    pub employee_account: Account<'info, EmployeeAccount>,
+    #[account(
+        mut,
+        seeds = [company_name.as_ref()],
+        bump = vesting_account.bump,
+        has_one = treasury_token_account,
+        has_one = mint
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(mut)]
+    pub treasury_token_account: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        init,
+        payer = beneficiary,
+        associated_token::mint = mint,
+        associated_token::authority = beneficiary,
+        associated_token::token_program = token_program
+    )]
+    pub employee_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace, Debug)]
+pub struct VestingAccount {
+    pub owner: Pubkey,
+    pub mint: Pubkey,
+    pub treasury_token_account: Pubkey,
+    #[max_len(50)]
+    pub company_name: String,
+    pub treasury_bump: u8,
+    pub bump: u8,
+}
+
+#[account]
+#[derive(InitSpace, Debug)]
+pub struct EmployeeAccount {
+    pub beneficiary: Pubkey,  // we need to know who will claim
+    pub start_time: i64,
+    pub end_time: i64,
+    pub total_amount: i64,
+    pub total_withdrawn: i64, 
+    pub cliff_time: i64,
+    pub vesting_account: Pubkey,
+    pub bump: u8,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Claiming is not available yet.")]
+    ClaimNotAvailableYet,
+    #[msg("There is nothing to claim.")]
+    NothingToClaim,
+}
